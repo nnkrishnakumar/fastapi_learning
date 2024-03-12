1557d8bd4ccfafc66cb4b29b5276bd1fe373e2b8
nnkrishnakumar <nnkrishna714@gmail.com>
2024-03-12 12:45:19 +0530

    dynamic route implementation

--- fastapi_code/main.py
+++ fastapi_code/main.py
@@ -5,3 +5,20 @@ app=FastAPI()
 @app.get('/')
 def hello():
     return {"name":"Krishna"}
+
+@app.get("/about")
+def about():
+    return {"about":"this is about us page"}
+
+@app.get("/blog")
+def blog():
+    return {"blog":"this is the first blog"}
+
+@app.get("/blog_id/{id}")         #it is a dynamic route
+def blog_id(id):
+    return {'data':id}
+
+@app.get("/blog_id/{id}/comments")
+def comments(id):
+    return {'data':{'1','2'}}
+
---
+++ fastapi_code/test.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def hello():
+    return {"name": "Krishna"}
+
+@app.get("/about")
+def about():
+    return {"about": "this is about us page"}
+
+@app.get("/blog")
+def blog():
+    return {"blog": "this is the first blog"}
+
+@app.get("/blog_id/{id}")  # it is a dynamic route
+def blog_id(id):
+    return {'data': id}
+
+@app.get("/blog_id/{id}/comments")  # another dynamic route
+def comments(id):
+    return {'data': {'1', '2'}}
